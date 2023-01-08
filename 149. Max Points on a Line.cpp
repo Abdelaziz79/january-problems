88142d1ffd34637db4a5fa41a7d842b9b527cf9b
Your Name <you@example.com>
2023-01-08 16:03:16 +0200

    the problem of day 8

---
+++ 149. Max Points on a Line.cpp
@@ -0,0 +1,23 @@
+int maxPoints(vector<vector<int>>& points){
+
+    int n = points.size();
+
+    if(n==1) return 1;
+
+    int res = 2;
+
+    for (int i = 0; i < n; ++i) {
+        unordered_map<double,int> cnt;
+        for (int j = 0; j < n; ++j) {
+            if(i!=j){
+                cnt[atan2(points[j][1]-points[i][1],points[j][0]-points[i][0])]++;
+            }
+        }
+        for(auto [h,count]:cnt){
+            res = max(res,count+1);
+        }
+    }
+
+    return res;
+}
+
